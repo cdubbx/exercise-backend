12cf36575cebd59b61871d77292f950552f9093a
cdubbx <124006449+cdubbx@users.noreply.github.com>
2025-02-13 11:33:49 -0500

    adding static to settings.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.0.13', ' 192.168.0.8',  '*' 
 
 # Application definition
 
+
+# him
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -199,8 +201,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
