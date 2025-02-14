168bf83547342fe68b21cba88810977bf4017d7d
cdubbx <124006449+cdubbx@users.noreply.github.com>
2025-02-13 19:13:01 -0500

    fixing the asgi file

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -14,7 +14,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from backend.backend1.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
