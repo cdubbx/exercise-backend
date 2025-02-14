59c3221adb4c426847e1c98c84cdaf4f6b5886c7
cdubbx <124006449+cdubbx@users.noreply.github.com>
2025-02-13 19:08:10 -0500

    fixing paths.

--- backend/backend1/routing.py
+++ backend/backend1/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from backend1.consumers import SpotifyConsumer
+from .consumers import SpotifyConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/spotify/(?P<username>\w+)/$", SpotifyConsumer.as_asgi())
